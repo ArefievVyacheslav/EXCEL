954455716403799e52ec4a7c2f3733ad21b6e155
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2022-12-04 06:14:22 +0300

    finish base framework functional

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './scss/index.scss'
+import { Excel } from '@/components/excel/Excel'
+import { Header } from '@/components/header/Header'
+import { Formula } from '@/components/formula/Formula'
+import { Toolbar } from '@/components/toolbar/Toolbar'
+import { Table } from '@/components/table/Table'
+
+const excel = new Excel('#app', {
+  components: [ Header, Toolbar, Formula, Table ]
+})
+
+console.log('excel', excel.render())
